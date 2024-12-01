1022338202a343904198abf9b6967e454a94ccac
mmmansouri <mansourimedmoez@gmail.com>
2024-12-01 22:01:33 +0100

    Initial commit

---
+++ src/test/java/com/bynature/ByNatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bynature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByNatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
